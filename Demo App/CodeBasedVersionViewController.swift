32a0793f66f2c786adc0405c1451716e8480526d
Alireza Asadi <alireza.asadi.36@gmail.com>
2019-04-30 19:32:24 +0430

    Initial commit. Storyboard and Code base map added.

---
+++ Demo App/CodeBasedVersionViewController.swift
@@ -0,0 +1,33 @@
+//
+//  CodeBasedVersionViewController.swift
+//  Demo App
+//
+//  Created by Alireza Asadi on 10/2/1398 AP.
+//  Copyright © 1398 AP MrAlirezaa. All rights reserved.
+//
+
+import UIKit
+import Mapbox
+
+class CodeBasedVersionViewController: UIViewController, MGLMapViewDelegate {
+    
+    var mapView: MGLMapView! {
+        didSet {
+            mapView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+            mapView.styleURL = MGLStyle.mapirVectorStyleURL()
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        // Instanciate MapView
+        mapView = MGLMapView(frame: view.bounds)
+                
+        view.addSubview(mapView)
+        
+        mapView.delegate = self
+    }
+}
+
---
+++ Demo App/StoryboardBasedVersionViewController.swift
@@ -0,0 +1,36 @@
+//
+//  StoryboardBasedVersionViewController.swift
+//  Demo App
+//
+//  Created by Alireza Asadi on 10/2/1398 AP.
+//  Copyright © 1398 AP MrAlirezaa. All rights reserved.
+//
+
+import UIKit
+import Mapbox
+
+class StoryboardBasedVersionViewController: UIViewController {
+
+    @IBOutlet weak var mapView: MGLMapView! {
+        didSet {
+            mapView.styleURL = MGLStyle.mapirVectorStyleURL()
+        }
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
