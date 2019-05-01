2cbddf47baf160e0907f509b8c40c11b11e7c7f0
Alireza Asadi <alireza.asadi.36@gmail.com>
2019-05-01 12:03:50 +0430

    Callouts Added

---
+++ Demo App/View Controllers/CalloutsViewController.swift
@@ -0,0 +1,53 @@
+//
+//  CalloutsViewController.swift
+//  Demo App
+//
+//  Created by Alireza Asadi on 11/2/1398 AP.
+//  Copyright © 1398 AP MrAlirezaa. All rights reserved.
+//
+
+import UIKit
+import Mapbox
+
+class CalloutsViewController: UIViewController, MGLMapViewDelegate {
+    
+    var mapView: MGLMapView! {
+        didSet {
+            mapView.styleURL = MGLStyle.mapirVectorStyleURL()
+            mapView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Creating object of Map View.
+        mapView = MGLMapView(frame: view.bounds)
+        view.addSubview(mapView)
+        
+        mapView.delegate = self
+        
+        // Making point annotations.
+        let annotationA = MGLPointAnnotation()
+        annotationA.coordinate = CLLocationCoordinate2D(latitude: 35.732547, longitude: 51.422682)
+        annotationA.title = "Map.ir"
+        annotationA.subtitle = "Savojinia, Tehran"
+        
+        let annotationB = MGLPointAnnotation()
+        annotationB.coordinate = CLLocationCoordinate2D(latitude: 35.744870, longitude: 51.375341)
+        annotationB.title = "Milad Tower"
+        annotationB.subtitle = "Tehran"
+        
+        // Adding annotaions to the mapView
+        mapView.addAnnotations([annotationA, annotationB])
+        
+        // Setting center of the map for better preview.
+        mapView.zoomLevel = 11
+        mapView.centerCoordinate = CLLocationCoordinate2D(latitude: 35.739383, longitude: 51.398894)
+    }
+    
+    // Delegate method to let the annotation show callouts.
+    func mapView(_ mapView: MGLMapView, annotationCanShowCallout annotation: MGLAnnotation) -> Bool {
+        return true
+    }
+}
