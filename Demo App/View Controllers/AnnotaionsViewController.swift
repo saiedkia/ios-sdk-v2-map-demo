d0ede5605faf0e548130d367b130bff30a8184ec
Alireza Asadi <alireza.asadi.36@gmail.com>
2019-05-01 11:32:58 +0430

    Annotations Added

--- Demo App/View Controllers/AnnotaionsViewController.swift
+++ Demo App/View Controllers/AnnotaionsViewController.swift
@@ -7,13 +7,35 @@
 //
 
 import UIKit
+import Mapbox
 
-class AnnotaionsViewController: UIViewController {
+class AnnotaionsViewController: UIViewController, MGLMapViewDelegate {
+    
+    var mapView: MGLMapView! {
+        didSet {
+            mapView.styleURL = MGLStyle.mapirVectorStyleURL()
+            mapView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        mapView = MGLMapView(frame: view.bounds)
+        view.addSubview(mapView)
+        
+        mapView.delegate = self
+        
+        let tehranCoordinates = CLLocationCoordinate2D(latitude: 35.6, longitude: 51.3)
+        
+        let annotaion = MGLPointAnnotation()
+        annotaion.coordinate = tehranCoordinates
+        mapView.addAnnotation(annotaion)
+        
+        mapView.zoomLevel = 2
+        mapView.centerCoordinate = tehranCoordinates
     }
     
 
