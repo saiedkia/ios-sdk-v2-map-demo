b2894e1b2b75196490e260bd34f55db83e9be1eb
Alireza Asadi <alireza.asadi.36@gmail.com>
2019-05-04 14:29:01 +0430

    Animated AnnotationView added

--- Demo App/View Controllers/AnimatedAnnotationsViewController.swift
+++ Demo App/View Controllers/AnimatedAnnotationsViewController.swift
@@ -18,7 +18,7 @@ class AnimatedAnnotationsViewController: UIViewController, MGLMapViewDelegate {
             mapView.minimumZoomLevel = 1
         }
     }
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -27,9 +27,53 @@ class AnimatedAnnotationsViewController: UIViewController, MGLMapViewDelegate {
         
         mapView.delegate = self
         
+        let coordinates = [CLLocationCoordinate2D(latitude: 35.0, longitude: 51.0),
+                           CLLocationCoordinate2D(latitude: 36.0, longitude: 52.0),
+                           CLLocationCoordinate2D(latitude: 34.0, longitude: 50.0)]
         
+        var annotations = [MGLPointAnnotation]()
+        
+        for coordinate in coordinates {
+            let annotation = MGLPointAnnotation()
+            annotation.coordinate = coordinate
+            annotation.title = "\(coordinate.latitude), \(coordinate.longitude)"
+            
+            annotations.append(annotation)
+        }
+        
+        mapView.addAnnotations(annotations)
+        
+        mapView.centerCoordinate = CLLocationCoordinate2D(latitude: 35.0, longitude: 51.0)
+        mapView.zoomLevel = 6
     }
-
+    
+    // - MARK: MGLMapViewDelegate methods
+    
+    func mapView(_ mapView: MGLMapView, annotationCanShowCallout annotation: MGLAnnotation) -> Bool {
+        return true
+    }
+    
+    func mapView(_ mapView: MGLMapView, viewFor annotation: MGLAnnotation) -> MGLAnnotationView? {
+        guard annotation is MGLPointAnnotation else {
+            return nil
+        }
+            
+        let reuseIdentifier = "\(annotation.coordinate.latitude), \(annotation.coordinate.longitude)"
+        
+        var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: reuseIdentifier) as? AnimatedAnnotationView
+        
+        if annotationView == nil {
+            annotationView = AnimatedAnnotationView(reuseIdentifier: reuseIdentifier)
+            
+            annotationView?.bounds = CGRect(x: 0, y: 0, width: 40, height: 40)
+            
+            let hue = CGFloat(annotation.coordinate.longitude.remainder(dividingBy: 50)) / 10
+            annotationView?.backgroundColor = UIColor(hue: hue, saturation: 0.5, brightness: 1, alpha: 1)
+        }
+        
+        return annotationView
+    }
+    
 }
 
 class AnimatedAnnotationView: MGLAnnotationView {
