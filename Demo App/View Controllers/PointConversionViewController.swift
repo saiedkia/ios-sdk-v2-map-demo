4f961767379bda6e36cee085da951c6829ea2c83
Alireza Asadi <alireza.asadi.36@gmail.com>
2019-05-04 17:22:05 +0430

    Restricting Map Panning Added

---
+++ Demo App/View Controllers/PointConversionViewController.swift
@@ -0,0 +1,59 @@
+//
+//  PointConversionViewController.swift
+//  Demo App
+//
+//  Created by Alireza Asadi on 14/2/1398 AP.
+//  Copyright © 1398 AP MrAlirezaa. All rights reserved.
+//
+
+import UIKit
+import Mapbox
+
+class PointConversionViewController: UIViewController, MGLMapViewDelegate {
+
+    var mapView: MGLMapView! {
+        didSet {
+            mapView.styleURL = MGLStyle.mapirVectorStyleURL()
+            mapView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+            mapView.minimumZoomLevel = 1
+        }
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        mapView = MGLMapView(frame: view.bounds)
+        view.addSubview(mapView)
+        
+        mapView.delegate = self
+        
+//        let tapRecognizer = UIGestureRecognizer(target: self, action: #selector(handleMapTap()))
+        
+    }
+
+    @IBAction func handleMapTap(_ sender: UITapGestureRecognizer) {
+        switch sender.state {
+        case .ended:
+            let tapPoint = sender.location(in: mapView)
+            let tapCoordinate: CLLocationCoordinate2D = mapView.convert(tapPoint, toCoordinateFrom: nil)
+            
+            print("Tap Coordinate: \(tapCoordinate.latitude), \(tapCoordinate.longitude)")
+            
+            if mapView.annotations != nil, let existingAnnotations = mapView.annotations {
+                mapView.removeAnnotations(existingAnnotations)
+            }
+            
+            let annotation = MGLPointAnnotation()
+            annotation.coordinate = tapCoordinate
+            annotation.title = "\(tapCoordinate.latitude), \(tapCoordinate.longitude)"
+            
+            mapView.addAnnotation(annotation)
+            
+        default:
+            break
+        }
+    }
+    
+    func mapView(_ mapView: MGLMapView, annotationCanShowCallout annotation: MGLAnnotation) -> Bool {
+        return true
+    }
+}
---
+++ Demo App/View Controllers/RestrictMapPanningViewController.swift
@@ -0,0 +1,58 @@
+//
+//  RestrictMapPanningViewController.swift
+//  Demo App
+//
+//  Created by Alireza Asadi on 14/2/1398 AP.
+//  Copyright © 1398 AP MrAlirezaa. All rights reserved.
+//
+
+import UIKit
+import Mapbox
+
+class RestrictMapPanningViewController: UIViewController, MGLMapViewDelegate {
+
+    var mapView: MGLMapView! {
+        didSet {
+            mapView.styleURL = MGLStyle.mapirVectorStyleURL()
+            mapView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+            mapView.minimumZoomLevel = 1
+        }
+    }
+    
+    var iran: MGLCoordinateBounds!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        mapView = MGLMapView(frame: view.bounds)
+        view.addSubview(mapView)
+        
+        mapView.delegate = self
+        
+        let northwest = CLLocationCoordinate2D(latitude: 42.0, longitude: 66.0)
+        let southeast = CLLocationCoordinate2D(latitude: 22.0, longitude: 42.0)
+        
+        iran = MGLCoordinateBounds(sw: southeast, ne: northwest)
+        
+        mapView.zoomLevel = 7
+        mapView.centerCoordinate = CLLocationCoordinate2D(latitude: 35.631642, longitude: 51.349183)
+        
+    }
+    
+    func mapView(_ mapView: MGLMapView, shouldChangeFrom oldCamera: MGLMapCamera, to newCamera: MGLMapCamera) -> Bool {
+        
+        let currentCamera = mapView.camera
+        
+        let newCameraCenter = newCamera.centerCoordinate
+        
+        mapView.camera = newCamera
+        let newVisibleCoordinates = mapView.visibleCoordinateBounds
+        
+        mapView.camera = currentCamera
+        
+        let inside = MGLCoordinateInCoordinateBounds(newCameraCenter, self.iran)
+        let intersects = MGLCoordinateInCoordinateBounds(newVisibleCoordinates.ne, iran) && MGLCoordinateInCoordinateBounds(newVisibleCoordinates.sw, iran)
+        
+        return inside && intersects
+    }
+}
